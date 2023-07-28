b88207a613849dad18219c2dfb3071f184961230
Moritz Friedrich <moritz@matchory.com>
2023-07-28 10:45:43 +0200

    Initial commit

---
+++ app/Nova/Actions/TestQueuedAction.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use function sleep;
+
+class TestQueuedAction extends Action implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable;
+
+    public $timeout = 10;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param \Laravel\Nova\Fields\ActionFields $fields
+     * @param \Illuminate\Support\Collection $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        Log::info('TestQueuedAction::handle()');
+
+        sleep(10);
+
+        Log::info('TestQueuedAction::handle() - done');
+
+        return Action::message('Slept 10 seconds');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [];
+    }
+}
